dd7b9621a486476d342774c8230d9383c53412a9
Huskehhh <husky@oresomecraft.com>
2014-04-07 20:55:09 +1000

    Add tokens

--- src/com/huskehhh/dingocode/DingoUtil.java
+++ src/com/huskehhh/dingocode/DingoUtil.java
@@ -15,7 +15,7 @@ public class DingoUtil {
     public static MySQL mysql = new MySQL(config.getString("MySQL.host"), config.getString("MySQL.port"), config.getString("MySQL.database"), config.getString("MySQL.user"), config.getString("MySQL.password"));
 
     public static int getGems(Player p) {
-        String query = "SELECT gems FROM `table` WHERE name LIKE " + p.getName() + ";";
+        String query = "SELECT gems FROM `data` WHERE name LIKE " + p.getName() + ";";
         ResultSet rs = mysql.querySQL(query);
         try {
             if (!rs.next()) {
@@ -30,7 +30,27 @@ public class DingoUtil {
     }
 
     public static void setGems(Player p, int gems) {
-        String execute = "UPDATE gems FROM `table` WHERE name LIKE " + p.getName() + " VALUES (" + gems + ");";
+        String execute = "UPDATE gems FROM `data` WHERE name LIKE " + p.getName() + " VALUES (" + gems + ");";
+        mysql.updateSQL(execute);
+    }
+
+    public static int getTokens(Player p) {
+        String query = "SELECT tokens FROM `data` WHERE name LIKE " + p.getName() + ";";
+        ResultSet rs = mysql.querySQL(query);
+        try {
+            if (!rs.next()) {
+                setTokens(p, 0);
+            } else {
+                return rs.getInt("tokens");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
+    public static void setTokens(Player p, int tokens) {
+        String execute = "UPDATE tokens FROM `data` WHERE name LIKE " + p.getName() + " VALUES (" + tokens + ");";
         mysql.updateSQL(execute);
     }
 
